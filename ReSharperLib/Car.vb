06e5272d381f20e3545e8e26c87fa12f8c5053b2
Gabriel Weyer <gabrielweyer@gmail.com>
2015-01-20 15:23:53 +1100

    Initial version instead of modified version

--- ReSharperLib/Car.vb
+++ ReSharperLib/Car.vb
@@ -1,4 +1,4 @@
-﻿Public Class Vehicle
+﻿Public Class Car
     Private _isRunning As Boolean
 
     Public Property IsRunning As Boolean
@@ -18,19 +18,7 @@
         IsRunning = False
     End Sub
 
-End Class
-
-Public Class Car
-    Inherits Vehicle
-
     Public Sub GetTicketBySpeedingUp()
-    End Sub
-End Class
-
-Public Class Tractor
-    Inherits Vehicle
-
-    Public Sub Plow()
     End Sub
 
 End Class
--- ReSharperLib/Introduction.vb
+++ ReSharperLib/Introduction.vb
@@ -1,16 +1,15 @@
 ﻿Public Class Introduction
 
     Public Sub ComputeMean()
-        ComputeAverage(1, 2, 3, 5)
+        ComputeAverage(1, 2, 3)
     End Sub
 
-    Public Sub ComputeAverage(ByVal valueOne As Int32, ByVal valueTwo As Int32, ByVal valueThree As Int32, ByVal valueFour As Integer)
-        Dim index = valueOne + valueThree
-        Dim niceValue As Int32 = index + valueFour
+    Public Sub ComputeAverage(ByVal valueOne As Int32, ByVal valueTwo As Int32, ByVal valueThree As Int32)
+        Dim niceValue As Int32 = valueOne + valueThree + 5
 
-        Console.Write("{0}", index)
+        Console.Write("{0}", valueOne + valueThree)
 
-        Dim logOutput As String = String.Format("Value: {0}", index)
+        Dim logOutput As String = String.Format("Value: {0}", valueOne + valueThree)
     End Sub
 
 End Class
